300881905ea3f4686b5d0bc985cb7f1217d8fa9b
Jacob Herper <jacob@herper.uk>
2019-09-09 10:04:22 +0100

    Dynamic primary colour implemented

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,5 +1,7 @@
-import React from "react"
+import React, { useContext } from "react"
 import { ThemeProvider } from "styled-components"
+import ReportContext from "../../context/report.context"
+
 import "./layout.css"
 
 if (typeof window !== "undefined") {
@@ -14,32 +16,34 @@ interface IProps {
   children: React.ReactNode
 }
 
-const theme = {
-  breakpoints: {
-    desktop: `992px`,
-    tablet: `768px`,
-    phone: `576px`,
-  },
-  container: {
-    width: `966px`,
-    maxWidth: `90%`,
-  },
-  colors: {
-    black: `#000000`,
-    darkGrey: `#404040`,
-    lightGrey: `#f6f5f5`,
-    mediumGrey: `#d8d8d8`,
-    primary: `#040095`, // (context: any) => context.report.primaryColor
-    secondary: `#e64150`,
-    white: `#ffffff`,
-  },
-  fonts: {
-    tiempos: `"Tiempos", serif`,
-    apercu: `"Apercu", sans-serif`,
-  },
-}
-
 const Layout = ({ children }: IProps) => {
+  const { report } = useContext(ReportContext)
+
+  const theme = {
+    breakpoints: {
+      desktop: `992px`,
+      tablet: `768px`,
+      phone: `576px`,
+    },
+    container: {
+      width: `966px`,
+      maxWidth: `90%`,
+    },
+    colors: {
+      black: `#000000`,
+      darkGrey: `#404040`,
+      lightGrey: `#f6f5f5`,
+      mediumGrey: `#d8d8d8`,
+      primary: report ? report.color : `#333`, // (context: any) => context.report.primaryColor
+      secondary: `#e64150`,
+      white: `#ffffff`,
+    },
+    fonts: {
+      tiempos: `"Tiempos", serif`,
+      apercu: `"Apercu", sans-serif`,
+    },
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <React.Fragment>
